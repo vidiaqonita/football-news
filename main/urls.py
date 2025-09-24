74c65a43894f64343c560ab4b7877a75078215ba
vidiaqonita <qonitavidia@gmail.com>
2025-09-24 15:53:01 +0700

    Tutorial 4

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from main.views import show_main, create_news, show_news, show_xml, show_json, s
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
+from main.views import edit_news
+from main.views import delete_news
 app_name = 'main'
 
 urlpatterns = [
@@ -16,4 +18,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('news/<uuid:id>/edit', edit_news, name='edit_news'),
+    path('news/<uuid:id>/delete', delete_news, name='delete_news'),
 ]
